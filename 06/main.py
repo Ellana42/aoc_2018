03a647a33cb5b70f3e6e8bb18a5e094375346151
Ellana42 <mtmrkaploun@gmail.com>
2023-05-26 16:41:40 +0200

    p2

--- 06/main.py
+++ 06/main.py
@@ -1,6 +1,8 @@
 from collections import defaultdict
 from typing import List
 
+MAX_DIST = 10000
+
 file = open("input").read().split("\n")[:-1]
 coordinates = [tuple(int(num) for num in line.split(", ")) for line in file]
 print(coordinates)
@@ -25,6 +27,18 @@ def find_closest_point(target: tuple, points: List[tuple]):
     return (closest_point, tied)
 
 
+def is_close(target: tuple, points: List[tuple]):
+    total_dist = 0
+    for point in points:
+        dist = manhattan_distance(point, target)
+        total_dist += dist
+        if total_dist >= MAX_DIST:
+            return False
+    return True
+
+
+print("Part 1:")
+
 infinite = set()
 areas = defaultdict(int)
 
@@ -41,6 +55,7 @@ for x in range(400):
         else:
             areas[closest_point] += 1
 
+
 print(f"areas : {areas}")
 
 print(f"infinite : {infinite}")
@@ -48,3 +63,11 @@ print(f"infinite : {infinite}")
 biggest_area = max(areas.values())
 
 print(biggest_area)
+
+print("Part 2:")
+
+close_points = sum(
+    is_close((x, y), coordinates) for x in range(400) for y in range(400)
+)
+
+print(f"Answer : {close_points}")
