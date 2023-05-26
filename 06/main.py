8dfab5a76b63a90c8c919379194f6335cdd1ba6a
Ellana42 <mtmrkaploun@gmail.com>
2023-05-26 15:33:02 +0200

    p1

---
+++ 06/main.py
@@ -0,0 +1,50 @@
+from collections import defaultdict
+from typing import List
+
+file = open("input").read().split("\n")[:-1]
+coordinates = [tuple(int(num) for num in line.split(", ")) for line in file]
+print(coordinates)
+
+
+def manhattan_distance(point1, point2):
+    return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
+
+
+def find_closest_point(target: tuple, points: List[tuple]):
+    min_distance = 500
+    closest_point = points[0]
+    tied = False
+    for point in points:
+        dist = manhattan_distance(point, target)
+        if dist == min_distance:
+            tied = True
+        if dist < min_distance:
+            min_distance = dist
+            closest_point = point
+            tied = False
+    return (closest_point, tied)
+
+
+infinite = set()
+areas = defaultdict(int)
+
+for x in range(400):
+    for y in range(400):
+        closest_point, tied = find_closest_point((x, y), coordinates)
+        if tied:
+            continue
+        if closest_point in infinite:
+            continue
+        if x == 0 or x == 399 or y == 0 or y == 399:
+            infinite.add(closest_point)
+            areas[closest_point] = 0
+        else:
+            areas[closest_point] += 1
+
+print(f"areas : {areas}")
+
+print(f"infinite : {infinite}")
+
+biggest_area = max(areas.values())
+
+print(biggest_area)
