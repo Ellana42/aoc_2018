99e9221fd4b0e73393ec6c61726c4c576320c52f
Ellana42 <mtmrkaploun@gmail.com>
2023-05-26 12:43:31 +0200

    part 2

--- 05/main.py
+++ 05/main.py
@@ -1,3 +1,5 @@
+import string
+
 file = open("input").read()[:-1]
 print(file)
 
@@ -12,13 +14,33 @@ def is_polarised(letter1: str, letter2: str):
     return False
 
 
-result = []
+def remove_polymer(file: str, letter: str):
+    file = file.replace(letter.lower(), "")
+    file = file.replace(letter.upper(), "")
+    return file
 
-for letter in file:
-    if len(result) > 0 and is_polarised(result[-1], letter):
-        result.pop()
-    else:
-        result.append(letter)
 
+def reduce_polymer(file):
+    result = []
+    for letter in file:
+        if len(result) > 0 and is_polarised(result[-1], letter):
+            result.pop()
+        else:
+            result.append(letter)
+    return result
+
+
+result = reduce_polymer(file)
 print("".join(result))
 print(f"Answer 1: {len(result)}")
+
+best_letter, size = "a", len(file)
+
+for letter in string.ascii_lowercase:
+    reduced_file = remove_polymer(file, letter)
+    reduced_file = reduce_polymer(reduced_file)
+    if len(reduced_file) < size:
+        best_letter, size = letter, len(reduced_file)
+
+print(f"Best letter : {best_letter}")
+print(f"Answer 2: {size}")
